510f70844231bf8c4f33d017fe4ec5e41190582e
Quang Ngoc Thuy <quangthuy.dev.game@gmail.com>
2024-07-20 14:27:50 +0700

    Update light

---
+++ Assets/Scripts/Test/PowerPoles.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PowerPoles : MonoBehaviour,IConnection
+{
+    private HingeJoint2D Joint2D;
+    [SerializeField] private Color onLight, offLight;
+    
+    private void Start()
+    {
+        Joint2D = GetComponent<HingeJoint2D>();
+        Joint2D.connectedBody = GetComponent<Rigidbody2D>();
+        transform.GetChild(0).GetComponent<SpriteRenderer>().color = offLight;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out EndRope endRope))
+        {
+            Connect(endRope.gameObject);
+        }
+    }
+
+    public void Connect(GameObject any)
+    {
+        Joint2D.connectedBody = any.GetComponent<Rigidbody2D>();
+        // thong bao win o day
+        transform.GetChild(0).GetComponent<SpriteRenderer>().color = onLight;
+    }
+
+    public void Disconnect()
+    {
+        
+    }
+}
