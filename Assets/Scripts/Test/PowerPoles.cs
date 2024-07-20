431c55c1454d2095f7710f4e58dd0c31583831c9
DangChiLuat <dangchiluat001@gmail.com>
2024-07-20 21:05:33 +0700

    update

--- Assets/Scripts/Test/PowerPoles.cs
+++ Assets/Scripts/Test/PowerPoles.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class PowerPoles : MonoBehaviour,IConnection
 {
+    [SerializeField] private GameObject UIW;
     private HingeJoint2D Joint2D;
     [SerializeField] private Color onLight, offLight;
     
@@ -28,6 +29,7 @@ public class PowerPoles : MonoBehaviour,IConnection
     {
         Joint2D.connectedBody = any.GetComponent<Rigidbody2D>();
         // thong bao win o day
+        UIW.SetActive(true);
         AudioManager.instance.PlaySFX(100, null);
         transform.GetChild(0).GetComponent<SpriteRenderer>().color = onLight;
     }
---
+++ Assets/Scripts/UI/UI.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class UI : MonoBehaviour
+{   
+    public void PlayerAgain()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
